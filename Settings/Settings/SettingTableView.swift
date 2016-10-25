8caed3860aa82ed36cc198cdab09aac4c078e9e3
Your Name <you@example.com>
2016-10-25 13:39:09 +0800

    SettingTableView add Constraint All screen

--- Settings/Settings/SettingTableView.swift
+++ Settings/Settings/SettingTableView.swift
@@ -38,6 +38,7 @@ public class SettingTableView: UITableView {
         self.setDelegate(delegate: content.delegate)
         self.allowsSelection = content.allowsSelection
         self.dataSource = dataSourcePtr
+        self.translatesAutoresizingMaskIntoConstraints = false
     }
 
     func setDelegate(delegate: UITableViewDelegate?){
@@ -48,5 +49,21 @@ public class SettingTableView: UITableView {
     public required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+
+    //MARK: AutoLayout
+    override public func layoutSubviews() {
+        updateConstraints()
+        super.layoutSubviews()
+    }
+
+    override public func updateConstraints() {
+        superview?.addConstraint(NSLayoutConstraint(item: self, attribute: .trailing, relatedBy: .equal, toItem: superview , attribute: .trailing, multiplier: 1, constant: 0))
+        superview?.addConstraint(NSLayoutConstraint(item: self, attribute: .leading, relatedBy: .equal, toItem: superview , attribute: .leading, multiplier: 1, constant: 0))
+        superview?.addConstraint(NSLayoutConstraint(item: self, attribute: .height, relatedBy: .equal, toItem: superview , attribute: .height, multiplier: 1, constant: 0))
+        superview?.addConstraint(NSLayoutConstraint(item: self, attribute: .centerY, relatedBy: .equal, toItem: superview , attribute: .centerY, multiplier: 1, constant: 0))
+
+        super.updateConstraints()
+    }
+
 }
 
